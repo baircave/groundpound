deef7e3db9c392d857261c5be3e18ca0ff9ac59f
Nick Newhouse <nick.newhouse@gmail.com>
2018-08-07 10:41:15 -0400

    Refactor some navbar code and add prof image gradient to navbar

--- frontend/components/main_page/navbar.jsx
+++ frontend/components/main_page/navbar.jsx
@@ -3,45 +3,56 @@ import { connect } from 'react-redux';
 import { logout, login } from '../../actions/session_actions';
 import { NavLink, withRouter } from 'react-router-dom';
 import { openModal } from '../../actions/modal_actions';
+import { generateRGB } from '../../util/helpers';
 
+const gradientString = `linear-gradient(45deg, #43c3d3, ${generateRGB()})`;
+
+const redirectToProfile = (userId, history) => {
+  history.push(`/users/${userId}`);
+};
 
 const Navbar = ({ user, login, logout, loggedIn, visible, openModal, history }) => {
   if (!visible) {
     return null;
   }
 
-  let navbar;
+  let profileImage = <div className="navbar-prof-photo"
+    style={ {background: gradientString}}></div>;
+
+  let navbarButtons = (
+    <div className="navbar-buttons">
+      <button className="trans-button session-navbar-button"
+        onClick={() => openModal("login")}>Sign in</button>
+      <button className="color-button session-navbar-button"
+        onClick={() => openModal("signup")}>Create account</button>
+      <button className="trans-button session-navbar-button"
+        onClick={() => login({username: 'guest', password: 'asdfasdf'})}>Guest</button>
+    </div>
+  );
 
   if (loggedIn) {
-    navbar = (
-      <div className="navbar">
-        <div className="navbar-content">
-          <img onClick={ () => history.push('/') } src={window.gp_logo}/>
-          <input type="text" placeholder="Search"></input>
-          <NavLink className="nav-button" to="/upload">Upload</NavLink>
-          <NavLink className="nav-button" to={`/users/${user.id}`}>{user.username}</NavLink>
-          <button className="nav-button" onClick={logout}>Sign Out</button>
+    navbarButtons = (
+      <div className="navbar-buttons">
+        <NavLink className="nav-button" to="/upload">Upload</NavLink>
+        <div className="profile-menu-button nav-button"
+          onClick={() => redirectToProfile(user.id, history)}>
+          {profileImage}
+          <button className="nav-button prof-button">{user.username}</button>
         </div>
-      </div>
-    );
-  } else {
-    navbar = (
-      <div className="navbar">
-        <div className="navbar-content">
-          <img onClick={ () => history.push('/') } src={window.gp_logo}/>
-          <input type="text" placeholder="Search"></input>
-          <button className="trans-button session-navbar-button"
-            onClick={() => openModal("login")}>Sign in</button>
-          <button className="color-button session-navbar-button"
-            onClick={() => openModal("signup")}>Create account</button>
-          <button className="trans-button session-navbar-button"
-            onClick={() => login({username: 'guest', password: 'asdfasdf'})}>Guest</button>
-        </div>
+        <button className="nav-button" onClick={logout}>Sign Out</button>
       </div>
     );
   }
 
-  return navbar;
+  return (
+    <div className="navbar">
+      <div className="navbar-content">
+        <img onClick={ () => history.push('/') } src={window.gp_logo}/>
+        <input type="text" placeholder="Search"></input>
+        {navbarButtons}
+      </div>
+    </div>
+  );
 };
 
 const mapStateToProps = (state) => {
